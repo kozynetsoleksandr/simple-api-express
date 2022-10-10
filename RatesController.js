fb15373b332cce4e4c0712b09af945712b0693bf
kozynetsoleksandr <75185598+kozynetsoleksandr@users.noreply.github.com>
2022-10-10 08:27:09 +0300

    Update README.MD

--- RatesController.js
+++ RatesController.js
@@ -3,14 +3,11 @@ import RatesService from "./RatesService.js";
 class RatesController {
     async index(req, res) {
         try {
-            const currencyInfo = await RatesService.findCurrency(req.query.currency);            
-            if (currencyInfo.data) {
-                res.json({usd: currencyInfo.data.rateUsd});
-            } else {
-                res.status(404).json({
-                    message: `incorrect currency name ${req.query.currency}` 
-                })
-            }
+            const currencyInfo = await RatesService.findCurrency(req.query.currency);
+            
+            (currencyInfo.data) ? res.json({usd: currencyInfo.data.rateUsd}) : res.status(404).json({
+                message: `incorrect currency name ${req.query.currency}` 
+            })
         } catch (error) {
             res.status(500).json(error);
         }
